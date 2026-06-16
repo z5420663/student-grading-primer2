9bb43089f75f39260701404de6502e9257af2a5c
z5420663 <z5420663@ad.unsw.edu.au>
2026-06-16 20:46:24 +1000

    feat: Initialise docker from pre-seed script and implement function stubs

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, abort, Response
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 import db
@@ -19,7 +19,7 @@ def get_students():
     """
     # TODO: replace with your implementation. This is a mock response
 
-    return jsonify(db.get_all_students())
+    return jsonify(db.get_all_students()), 200
   #  return jsonify([
   #      {'course': 'COMP1531', 'id': 1, 'mark': 85, 'name': 'Alice Zhang'},
   #      {'course': 'COMP1531', 'id': 2, 'mark': 72, 'name': 'Bob Smith'}
@@ -35,11 +35,13 @@ def create_student():
     param mark: The mark the student received (from request body)
     return: The created student if successful
     """
-
     # Getting the request body - replace with your implementation
-    student_data = request.json
-    db.insert_student(student_data["name"], student_data["course"], student_data["mark"])
-    return Response( response=student_data, status=200)
+    try:
+        student_data = request.json
+        s = db.insert_student(student_data["name"], student_data["course"], student_data.get("mark"))
+        return jsonify(s), 200
+    except Exception as e:
+        return str(e), 404
 
 
 
@@ -52,9 +54,13 @@ def update_student(student_id):
     param mark: The mark the student received (from request body)
     return: The updated student if successful
     """
-    student_data = request.json
-    db.update_student(student_id, student_data["name"], student_data["course"], student_data["mark"])
-    return db.get_student_by_id(student_id)
+    try:
+        student_data = request.json
+        if db.update_student(student_id, student_data["name"], student_data["course"], student_data.get("mark")) is None:
+            return "id not found", 404
+        return jsonify(db.get_student_by_id(student_id)), 200
+    except Exception as e:
+        return str(e) , 404
 
 
 
@@ -65,8 +71,9 @@ def delete_student(student_id):
     return: The deleted student
     """
     student_data = db.get_student_by_id(student_id)
-    db.delete_student(student_id)
-    return student_data
+    if db.delete_student(student_id) is None:
+        return "id not found", 404
+    return student_data, 200
     
 
 
@@ -78,10 +85,28 @@ def get_stats():
     """
     try:
         students = db.get_all_students()
-        scores = [ s["mark"] for s in students]
-        return (len(students), sum(scores)/len(scores), min(scores), max(scores))
-    except Exception:
-        return abort(404)
+        scores = []
+        if len(students) == 0:
+            return jsonify(
+                {
+                    "count":0,
+                    "average": 0,
+                    "min": 0,
+                    "max": 0}), 200
+            
+        for s in students:
+            m = s.get("mark")
+            if m:
+                scores.append(m)
+        return jsonify(
+                {
+                    "count":len(scores), 
+                    "average":sum(scores)/len(scores), 
+                    "min": min(scores), 
+                    "max": max(scores)}), 200
+
+    except Exception as e:
+        return (str(e), (404))
 
 
 
